54291cacd3a5c272975dbebc19ecf494792e2b4e
mchiari <mchiaridev@gmail.com>
2022-02-24 18:55:12 -0300

    first release

---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = "805e9af5";
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.scss"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div>MovieApp</div>
+      <div>©2022, Movie, Inc. or its affiliates</div>    
+    </div>
+  )
+}
+
+export default Footer
